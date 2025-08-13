5b5c0c9cde0e8dcf77a52db478efaae0d9da0ebb
Subhadragp <subhadragope27@gmail.com>
2025-08-13 13:24:12 +0530

    initial commit, learning sb from engineering digest

---
+++ src/main/java/com/subcodes/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,23 @@
+package com.subcodes.journalApp.controller;
+
+import com.subcodes.journalApp.model.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping("/")
+    public List<JournalEntry> getJournalEntries() {
+        return new ArrayList<JournalEntry>(journalEntries.values());
+    }
+}
---
+++ src/main/java/com/subcodes/journalApp/model/JournalEntry.java
@@ -0,0 +1,12 @@
+package com.subcodes.journalApp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+}
