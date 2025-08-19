31d808d7194128ebad0209db2b19044dfabc2a75
Subhadragp <subhadragope27@gmail.com>
2025-08-19 11:52:02 +0530

    implemented springsecurity, created security config, userDetailsServiceImpl

---
+++ src/main/java/com/subcodes/journalApp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.subcodes.journalApp.service;
+
+import com.subcodes.journalApp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByUserName(username);
+        if (user == null) {
+            return org.springframework.security.core.userdetails.User
+                    .builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
+        }
+        throw new UsernameNotFoundException("User not found with username: " + username);
+    }
+}
--- src/main/java/com/subcodes/journalApp/service/UserService.java
+++ src/main/java/com/subcodes/journalApp/service/UserService.java
@@ -4,8 +4,11 @@ import com.subcodes.journalApp.model.User;
 import com.subcodes.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +18,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void saveNewUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
     }
 
