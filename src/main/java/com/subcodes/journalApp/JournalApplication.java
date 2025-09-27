54019a19b9efe8d96ef80e572a334d32b8e2be73
Subhadragp <subhadragope27@gmail.com>
2025-09-27 21:30:56 +0530

    use .env variables when running in local

--- src/main/java/com/subcodes/journalApp/JournalApplication.java
+++ src/main/java/com/subcodes/journalApp/JournalApplication.java
@@ -1,5 +1,6 @@
 package com.subcodes.journalApp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,17 @@ import org.springframework.web.client.RestTemplate;
 public class JournalApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("APP_PORT", String.valueOf(dotenv.get("PORT")));
+		System.setProperty("SERVER_PORT_DEV", String.valueOf(dotenv.get("SERVER_PORT_DEV")));
+		System.setProperty("SERVER_PORT_PROD", String.valueOf(dotenv.get("SERVER_PORT_prod")));
+		System.setProperty("EMAIL_USERNAME", String.valueOf(dotenv.get("EMAIL_USERNAME")));
+		System.setProperty("EMAIL_APP_PASSWORD", String.valueOf(dotenv.get("EMAIL_APP_PASSWORD")));
+		System.setProperty("WEATHER_API_KEY", String.valueOf(dotenv.get("WEATHER_API_KEY")));
+		System.setProperty("MONGO_DB_URI", String.valueOf(dotenv.get("MONGO_DB_URI")));
+		System.setProperty("MONGO_USERNAME", String.valueOf(dotenv.get("MONGO_USERNAME")));
+		System.setProperty("MONGO_PASSWORD", String.valueOf(dotenv.get("MONGO_PASSWORD")));
+
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
